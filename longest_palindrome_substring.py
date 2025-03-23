a15aa9bee20f93c3538413653c6fd48e4d9e0251
alcho-prof <sandeepabhimanyu0@gmail.com>
2025-03-23 11:43:34 +0530

    longest palindrome substring

---
+++ longest_palindrome_substring.py
@@ -0,0 +1,8 @@
+def longest_palindrome_substring():
+    for i in range(len(s)):
+        for j in range(i,len(s)):
+            a=s[i:j+1]
+        
+    return()      
+s=input()
+print(longest_palindrome_substring())        
