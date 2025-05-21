c0c1d1755cd04a080faf5a1b882a43040190621c
sndy <162935950+alcho-prof@users.noreply.github.com>
2025-05-21 07:46:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.07%) - LeetSync

--- 742-to-lower-case/to-lower-case.py
+++ 742-to-lower-case/to-lower-case.py
@@ -1,3 +1,12 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.casefold()
+        #return s.casefold()
+        res=''
+        for char in s:
+            if "A"<=char<="Z":
+                lower=chr(ord(char)+32)
+                res+=lower
+            else:
+                res+=char
+        return res            
+        
