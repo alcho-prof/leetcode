75081293303ebac74600c003b5df76313094f2bd
sndy <162935950+alcho-prof@users.noreply.github.com>
2025-06-05 22:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (8.17%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,10 +4,8 @@ class Solution:
             return ""
         a=strs[0]
         for i in strs[1:]:
-            j=0
-            while j<len(a) and j<len(i) and a[j]==i[j]:
-                j+=1
-            a=a[:j]
-            if not a:
-                return ""
-        return a            
+            while not i.startswith(a):
+                a=a[:-1]
+                if not a:
+                    return ""
+        return a                
