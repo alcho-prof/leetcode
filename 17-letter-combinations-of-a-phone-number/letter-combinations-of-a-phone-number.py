a8338147e50832335c53d8dec81178077ccc64ed
sndy <162935950+alcho-prof@users.noreply.github.com>
2025-09-05 16:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (23.26%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,31 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        # dictionary mapping
+        phone_map = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        res = []
+
+        def backtrack(index, path):
+            # base case
+            if index == len(digits):
+                res.append(path)
+                return
+
+            # dictionary la mapping check panni iterate pannuvom
+            for char in phone_map[digits[index]]:
+                backtrack(index + 1, path + char)
+
+        backtrack(0, "")
+        return res
