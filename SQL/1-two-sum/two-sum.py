f8dad7816159d4533725ebb22c777467f8cbcb36
sndy <162935950+alcho-prof@users.noreply.github.com>
2026-02-04 07:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (10.93%) - LeetSync

--- SQL/1-two-sum/two-sum.py
+++ SQL/1-two-sum/two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen={}
         for i,num in enumerate(nums):
-            res=target-num
-            if res in seen:
-                return[seen[res],i]
+            diff=target-num
+            if diff in seen:
+                return [seen[diff],i]
             seen[num]=i
